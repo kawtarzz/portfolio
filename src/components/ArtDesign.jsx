81707490084ccbba6ebf3c6766487da73df1cdf5
Kawtarzz <kawtaryazzouzi@gmail.com>
2024-06-19 12:10:02 -0500

    portfolio refined

--- src/components/ArtDesign.jsx
+++ src/components/ArtDesign.jsx
@@ -5,10 +5,7 @@ import RiverinParadisePreview from './Portfolio/RiverinParadise_Preview';
 import ShopTheBlockPreview from './Portfolio/ShoptheBlock_Preview';
 import CandyCartPreview from './Portfolio/CandyCart_Preview';
 import WitchesWizardsPreview from './Portfolio/WitchesWizards_Preview';
-import NormalCoffeePreview from './Portfolio/NormalCoffee_Preview';
-import GrandmasKitchenPreview from './Portfolio/GrandmasKitchen_Preview';
 import RocketMortgagePreview from './Portfolio/RocketMortgagePreview';
-import SpiritualTeensPreview from './Portfolio/SpiritualTeens_Preview';
 
 const ArtDesign = () => {
     return (
@@ -20,10 +17,7 @@ const ArtDesign = () => {
                 <ShopTheBlockPreview />
                 <CandyCartPreview />
                 <WitchesWizardsPreview />
-                <NormalCoffeePreview />
-                <GrandmasKitchenPreview /> 
                 <RocketMortgagePreview />
-                <SpiritualTeensPreview /> 
             </div>
 
         </>
